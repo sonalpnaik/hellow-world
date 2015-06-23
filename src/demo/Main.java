fc01b4bf1c0114b3f0ddecb54fe3edbf920bdc3a
komalnaik <komalnaik2408@gmaiol.com>
2015-06-23 16:45:15 +0530

    Update Main.java

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -9,6 +9,7 @@ public class Main
 		System.out.println("Hello World");
 		System.out.println("Hello");
 		System.out.println("Heyyy Friendzzzz! :)")
+		System.out.println("****************************************");
 
 	}
 
